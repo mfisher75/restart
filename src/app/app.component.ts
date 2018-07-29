98b84c8a4e942319c7f8ca8b399ed54d5fe26831
Moshe Fisher <mf840u@intl.att.com>
2018-07-29 14:18:32 +0300

    Move trips array to tripService and use ngOnInit in component to call it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,42 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TripDataService } from './trip-data.service';
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'RESTART';
   adding_trip = false;
+  trips = [];
 
   newTrip = {
-  	title: ""
+    title: ""
   }
 
-  trips = [{
-  	title: 'Trip to London'
-  }, {
-  	title: 'Trip to Australia'
-  } ,{
-  	title: 'Trip to Barcelona'
-  }];
+  constructor(private tripDataservice: TripDataService) {
+  }
+
+  ngOnInit() {
+    this.trips = this.tripDataservice.trips;
+
+  }
+
 
 
   addTrip() {
-  	this.adding_trip = true;
+    this.adding_trip = true;
   }
 
   cancelAddTrip() {
-  	console.log("cancelAddTrip");
-  	this.adding_trip = false;
+    console.log("cancelAddTrip");
+    this.adding_trip = false;
   }
 
   doAddTrip() {
-  	console.log("doAddTrip");
-  	this.trips.push(this.newTrip);
+    console.log("doAddTrip");
+    this.tripDataservice.trips.push(this.newTrip);
 
-  	this.newTrip = {title:""}
+    this.newTrip = { title: "" }
 
-  	this.adding_trip = false;
+    this.adding_trip = false;
   }
 }
--- src/app/trip-data.service.ts
+++ src/app/trip-data.service.ts
@@ -3,7 +3,18 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
-export class TripDataService {
+export class TripDataService{
+
+
+  trips = [{
+  	title: 'Trip to London'
+  }, {
+  	title: 'Trip to Australia'
+  } ,{
+  	title: 'Trip to Barcelona'
+  }];
 
   constructor() { }
+
+
 }
